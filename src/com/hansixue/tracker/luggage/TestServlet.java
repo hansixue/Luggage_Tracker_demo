63c987a32d91b3ce841f209a9f177085481a57cc
hansixue <hansixue@gmail.com>
2023-09-17 10:08:46 +0900

    init

---
+++ src/com/hansixue/tracker/luggage/TestServlet.java
@@ -0,0 +1,30 @@
+package com.hansixue.tracker.luggage;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+
+import java.io.IOException;
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
